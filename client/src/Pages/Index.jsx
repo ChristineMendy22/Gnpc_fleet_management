cc84d317faf99d06d02f610b7f01e2d951550543
Christine Mendy <christine.mendy@gnpc.local>
2026-07-14 15:39:22 +0000

    added the paragraph of right side

--- client/src/Pages/Index.jsx
+++ client/src/Pages/Index.jsx
@@ -11,20 +11,26 @@ const Index = () => {
   return (
     <div>
         <Navbar />
-        <div className="flex min-h-screen bg-gray-100">
+        <div className="flex min-h-screen bg-gray-100 lg:flex-row flex-col ">
             {/* Right side */}
             <div className="flex-1 p-6">
-                <p className="font-bold mb-4">Welcome to GNPC Gambia</p>
-                <h1 className="text-green-900 font-bold text-3xl">Vehicle Request & Fleet Utilization System </h1>
+                <p className="font-bold mb-4 text-green-900">Welcome to GNPC (Gambia)</p>
+                <h1 className="text-green-900 font-bold text-5xl md:text-6xl">Vehicle Request & Fleet Utilization System </h1>
+                <p className="mt-6 max-w-xl text-base md:text-lg text-gray-600 leading-8">
+                    Manage vehicle requests, approvals, drivers, and fleet operations in one centralized platform.
+                </p>
+                <button className="bg-green-900 text-white px-4 py-2 rounded-md flex items-center gap-2 hover:bg-green-700">
+                   <CarFront /> Request Vehicle
+                </button>
             </div>
 
             {/* Left Side */}
-            <div className="w-1/2 p-6 rounded-lg">
+            <div className="w-1/2 p-6 rounded-lg overflow-hidden">
                 <img src={PetroluemHouse} alt="Petroleum House" className="w-full h-auto rounded-lg" />
             </div>
         </div>
         {/* Grid Items */}
-        <div className='grid grid-cols-4 gap-4 p-6'>
+        <div className='grid lg:grid-cols-4 md:grid-cols-3  gap-4 p-6'>
             <div className='bg-white p-4 rounded-lg shadow-md flex items-center'>
                 <CarFront className='h-12 w-12 text-green-900 p-2 bg-green-100 rounded-full' />
                 <div className='ml-4'>
@@ -60,7 +66,7 @@ const Index = () => {
             </div>
         </div>
         <h2 className='px-6 text-2xl font-bold text-green-900 mb-4'>Quick Actions</h2>
-        <div className='grid grid-cols-4 gap-4 p-6'>
+        <div className='grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 gap-4 p-6'>
             <div className='border border-gray-200 p-4 rounded-lg flex items-center'>
                 <CarFront className='h-12 w-12 text-green-500 p-2 bg-green-100 rounded-full' />
                 <div className='ml-4'>
