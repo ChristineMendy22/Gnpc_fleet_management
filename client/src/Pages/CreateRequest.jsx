e53bb5fcb87c3783ffb7db21913a3e3128458966
Amadou Marong <marongamadou6@gmail.com>
2026-09-08 12:15:43 +0000

    updated the table styles

--- client/src/Pages/CreateRequest.jsx
+++ client/src/Pages/CreateRequest.jsx
@@ -4,7 +4,7 @@ import RequestForm from '../components/RequestForm';
 const NewRequests = () => {
   return (
     <AppLayout title="New Requests">
-      <div className="p-4 bg-gray-100 border-b border-gray-200 my-4">
+      <div className="p-4 bg-white border rounded-lg border-gray-200 my-4 shadow-md">
         <RequestForm />
       </div>
     </AppLayout>
